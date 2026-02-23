553e1989dea6f43fe17bf98d52efdbc2a48401f9
Djaroephon <djaroephon@gmail.com>
2026-02-23 23:35:25 +0700

    ze

--- commands/jadwal.js
+++ commands/jadwal.js
@@ -2,24 +2,24 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 const jadwalKuliah = {
     0: "Minggu Libur! Saatnya rebahan.", // Minggu
-    1: "Tidak ada jadwal hari ini. Semangat nugas!", // Senin
+    1: [
+        {mk:'Statistika', jam: '10:00 - 12:00', dosen: 'Dr. Mutiawati, S.Pd., M.Pd', klm: 'Ibuk' },
+    ], // Senin
     2: [ // Selasa
-         { mk: 'Scenario dan Design Game', jam: '08:00 - 11:00', dosen: 'Desita Ria Yusian TB, S.ST.,MT', klm: 'Ibuk' },
-        { mk: 'Basis Data', jam: '14:00 - 17:00', dosen: 'M. Bayu Wibawa, S.Kom., MMSI.', klm: 'bapak' }
+         { mk: 'Pemograman Berbasis Platform', jam: '08:00 - 11:00', dosen: 'Mahendar Dwi Payana, S.ST., MT', klm: 'bapak' },
+        { mk: 'Datawarehouse', jam: '14:00 - 17:00', dosen: 'M. Bayu Wibawa, S.Kom., MMSI.', klm: 'bapak' }
     ],
     3: [ // Rabu
-        { mk: 'Organisasi dan Arsitektur Komputer', jam: '08:00 - 12:00', dosen: 'Rizka Albar, S.Kom.,MT', klm: 'bapak' },
-        { mk: 'Pemrograman Berorientasi Objek', jam: '14:00 - 18:00', dosen: 'Mahendar Dwi Payana, S.ST.,MT', klm: 'bapak' }
+        { mk: 'Cyber Security', jam: '08:00 - 11:00', dosen: 'Rizka Albar, S.Kom.,MT', klm: 'bapak' },
     ],
     4: [ // Kamis
-        { mk: 'Leadership dan Entrepreneurship', jam: '08:00 - 11:00', dosen: 'Soraya Lestari, SE., M.Si', klm: 'Ibuk' },
-        { mk: 'Database Management System', jam: '14:00 - 17:00', dosen: 'M. Bayu Wibawa, S.Kom.,MMSI', klm: 'Bapak' }
+        { mk: 'Etika dan Berprofesi', jam: '14:00 - 17:00', dosen: 'Armia, ST.,M.Sc', klm: 'Bapak' }
     ],
-    5: "Tidak ada jadwal hari ini. Semangat nugas!", // Jumat
-    6: [ // Sabtu
-        { mk: 'Logika Matematika', jam: '10:00 - 13:00', dosen: 'Mahyus Ihsan, M.Si', klm: 'Bapak' },
-        { mk: 'Jaringan Komputer ', jam: '14:00 - 16:00', dosen: 'Rizka Albar, S.Kom.,MT', klm: 'Bapak' }
-    ]
+    5: [
+        {mk: 'Analisis dan Desain Perangkat Lunak', jam: '08:00 - 11:00', dosen: 'Miftahul Jannah, S.Kom., M.Pd', klm: 'Ibuk' },
+        {mk: 'Keamanan dan Informasi', jam: '14:00 - 17:00', dosen: 'Rizka Albar, S.Kom., MT', klm: 'bapak' }
+    ],
+    6: "Sabtu Libur! Saatnya main game atau nonton anime." 
 };
 // ---------------------------------
 
